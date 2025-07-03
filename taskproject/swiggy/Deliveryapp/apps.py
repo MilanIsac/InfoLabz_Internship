b386ae1770b3734d297dd65316f97aaa7b5f1819
Milan Isac <milan.isac.7@gmail.com>
2025-07-03 15:49:10 +0530

    error resolved

--- taskproject/swiggy/Deliveryapp/apps.py
+++ taskproject/swiggy/Deliveryapp/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class DeliveryappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Deliveryapp'
+    name = 'swiggy.Deliveryapp'
